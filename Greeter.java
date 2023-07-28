2b550104152803a96ca3f58973a30ad6a5af869e
Lukas Sutherland <lukas.sutherland71@gmail.com>
2023-07-28 21:02:23 +1200

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -20,7 +20,9 @@ public class Greeter {
     String language = args[0];
     String country = args[1];
     String name = args[2];
-    Greeter greeter = new Greeter(language, country, name);
-    System.out.println(greeter.sayHello());
+    if (args.length > 2) {
+      Greeter greeter = new Greeter(language, country, name);
+      System.out.println(greeter.sayHello());
+    }
   }
 }
